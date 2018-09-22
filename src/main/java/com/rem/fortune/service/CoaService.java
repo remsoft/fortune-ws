fb7f0746f2432cbe13e73491b550200898b2a484
remsoft <rem.soft.corp@gmail.com>
2018-09-22 12:27:07 +0800

    coa module

---
+++ src/main/java/com/rem/fortune/service/CoaService.java
@@ -0,0 +1,10 @@
+package com.rem.fortune.service;
+
+import java.util.List;
+
+import com.rem.fortune.model.Coa;
+
+public interface CoaService {
+	public List<Coa> getAllCoa();
+
+}
--- src/main/java/com/rem/fortune/service/CustomerSupplierService.java
+++ src/main/java/com/rem/fortune/service/CustomerSupplierService.java
@@ -2,12 +2,14 @@ package com.rem.fortune.service;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
+
 import com.rem.fortune.model.CustomerSupplier;
 
 public interface CustomerSupplierService {
-	public CustomerSupplier getById(String id) throws Exception;
-	public int create(CustomerSupplier custSupp) throws Exception;
-	public List<CustomerSupplier> getAllSupplier(int isCustomer) throws Exception;	
-	public int deleteById(int id) throws Exception;
-	public int update(CustomerSupplier custSup) throws Exception;
+	public CustomerSupplier getById(String id);
+	public int create(CustomerSupplier custSupp);
+	public List<CustomerSupplier> getAllSupplier(int isCustomer);	
+	public ResponseEntity deleteById(int id);
+	public ResponseEntity update(CustomerSupplier custSup);
 }
